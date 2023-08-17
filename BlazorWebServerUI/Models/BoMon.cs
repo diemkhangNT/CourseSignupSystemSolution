c4e490d26992ed4dff2f5558084b82333764c4b7
Nguyễn Thị Diễm Khang <diemkhang1912@gmail.com>
2023-08-17 15:34:56 +0700

    update_docker_vol_17/8

--- BlazorWebServerUI/Models/BoMon.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorWebServerUI.Models
-{
-    public class BoMon
-    {
-        public string MaBM { get; set; } 
-        public string TenBM { get; set; }
-    }
-}
--- BlazorWebServerUI/Models/LoaiDiem_MH.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorWebServerUI.Models
-{
-    public class LoaiDiem_MH
-    {
-        public string MaLDiem { get; set; }
-        public virtual LoaiDiem LoaiDiems { get; set; }
-        public string MaMH { get; set; }
-    }
-}
